11017789c0ea80d9211039d6758a7678a16ccc27
Daniel Einars <contact@dle.dev>
2021-03-19 22:51:19 +0100

    mostly up and running on local server
    Signed-off-by: Daniel Einars <contact@dle.dev>

--- certbot/docker.sh
+++ certbot/docker.sh
@@ -41,7 +41,7 @@ if [[ $(git rev-parse --abbrev-ref HEAD) == master ]]; then
   echo "***************************************************"
   echo "******* BUILDING PRODUCTION DOCKER CONTAINER ******"
   echo "***************************************************"
-  docker build . --no-cache -t "${TAG_LATEST}" --build-arg DIGITALOCEAN_ACCESS_TOKEN="$DIGITALOCEAN_ACCESS_TOKEN" -f ./Dockerfile
+  docker build . --no-cache -t "${TAG_LATEST}" --build-arg DIGITALOCEAN_ACCESS_TOKEN="$DIGITALOCEAN_ACCESS_TOKEN"  -f ./Dockerfile
   docker tag "${TAG_LATEST}" "${TAG_VERSION}"
 
   echo "***************************************************"
