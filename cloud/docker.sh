fe187e0050f7ffaaa4cce5fda5ea04246b7b309c
Daniel Einars <contact@dle.dev>
2022-07-02 22:37:21 +0200

    fix: nginx & cloudflare
    Signed-off-by: Daniel Einars <contact@dle.dev>

--- cloud/docker.sh
+++ cloud/docker.sh
@@ -2,8 +2,8 @@
 set -e
 while getopts p option; do
   case "${option}" in
-    p) PUSH=${OPTARG};;
-    *) INVALID=${OPTARG};;
+  p) PUSH=${OPTARG} ;;
+  *) INVALID=${OPTARG} ;;
   esac
 done
 
@@ -14,8 +14,6 @@ VERSION=$(npx -c 'echo "$npm_package_version"')
 TAG=${HOST}/${NAME}
 TAG_VERSION=${TAG}:${VERSION}
 TAG_LATEST=${TAG}:latest
-
-
 
 if [[ $(git rev-parse --abbrev-ref HEAD) == master ]]; then
   #  We are on master, only prod releases are possible here. Only the -p flag will be respected here.
@@ -32,24 +30,24 @@ if [[ $(git rev-parse --abbrev-ref HEAD) == master ]]; then
   echo "***************************************************"
   echo "************* PUSHING TO REPOSITORY ***************"
   echo "***************************************************"
-  # docker push "${TAG_LATEST}"
+  docker push "${TAG_LATEST}"
   docker push "${TAG_VERSION}"
 
 else
   #  We are on on a development branch. Commandline options -e and -r will be respected.
   echo "$TAG_LATEST"
 
+  echo "***************************************************"
+  echo "****** BUILDING DEVELOPMENT DOCKER CONTAINER ******"
+  echo "***************************************************"
+  docker build --no-cache . -t "${TAG_LATEST}" -f ./Dockerfile
+  docker tag "${TAG_LATEST}" "${TAG_VERSION}"
 
-   echo "***************************************************"
-   echo "****** BUILDING DEVELOPMENT DOCKER CONTAINER ******"
-   echo "***************************************************"
-   docker build --no-cache . -t "${TAG_LATEST}"  -f ./Dockerfile
-
-
-   if [[ -v PUSH ]]; then
-     echo "***************************************************"
-     echo "************* PUSHING TO REPOSITORY ***************"
-     echo "***************************************************"
-     docker push "${TAG_LATEST}"
-   fi
+  if [[ -v PUSH ]]; then
+    echo "***************************************************"
+    echo "************* PUSHING TO REPOSITORY ***************"
+    echo "***************************************************"
+    docker push "${TAG_LATEST}"
+    docker push "${TAG_VERSION}"
+  fi
 fi
--- cloudflared/scripts/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-exec sudo --preserve-env --set-home -u nonroot cloudflared --no-autoupdate tunnel run
