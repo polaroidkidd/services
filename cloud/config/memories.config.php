b79d457b0ba861a511c058c5c0fb2db8eb7c9b8a
Daniel Einars <contact@dle.dev>
2023-01-01 18:12:34 +0100

    feat: install ffmpeg into cloud container
    Signed-off-by: Daniel Einars <contact@dle.dev>

---
+++ cloud/config/memories.config.php
@@ -0,0 +1,9 @@
+<?php
+$CONFIG = array (
+  'memories.exiftool' => '/var/www/html/custom_apps/memories/exiftool-bin/exiftool-amd64-glibc',
+  'memories.ffmpeg_path' => '/usr/bin/ffmpeg',
+  'memories.ffprobe_path' => '/usr/bin/ffprobe',
+  'memories.transcoder' => '/var/www/html/custom_apps/memories/exiftool-bin/go-vod-amd64',
+  'memories.no_transcode' => true,
+  'memories.qsv' => false,
+);
