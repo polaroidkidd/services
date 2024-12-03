e87bc742969eb1c10b9cccbff0b52ff34bfe1742
Daniel Einars <contact@dle.dev>
2024-12-03 18:01:09 +0100

    feat: migrate to new HDD

--- server/dump-cloud-db.sh
+++ server/dump-cloud-db.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 
-docker-compose exec --user www-data cloud  php occ maintenance:mode --on
+docker compose exec --user www-data cloud  php occ maintenance:mode --on
 wait
-docker-compose restart cloud
+docker compose restart cloud
 wait
-docker exec -t dle-cloud-db pg_dumpall -c -U cloud  > /mnt/md0/storage/services/server/cloud-db-dumps/$(date +%Y-%m-%d_%H_%M_%S).sql
+docker exec -t dle-cloud-db pg_dumpall -c -U cloud  > /mnt/md1/storage/services/server/cloud-db-dumps/$(date +%Y-%m-%d_%H_%M_%S).sql
 wait
-docker-compose exec --user www-data cloud  php occ maintenance:mode --off
+docker compose exec --user www-data cloud  php occ maintenance:mode --off
 wait
-docker-compose restart cloud
+docker compose restart cloud
