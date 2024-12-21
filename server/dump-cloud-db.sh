b79329cadb66456f1c30619644d853f72a4b184f
Daniel Einars <contact@dle.dev>
2024-12-21 23:12:00 +0100

    fix: cloud dump script

--- server/dump-cloud-db.sh
+++ server/dump-cloud-db.sh
@@ -5,7 +5,7 @@ docker compose exec --user www-data cloud  php occ maintenance:mode --on
 wait
 docker compose restart cloud
 wait
-docker exec -t dle-cloud-db pg_dumpall -c -U cloud  > /mnt/md1/storage/services/server/cloud-db-dumps/$(date +%Y-%m-%d_%H_%M_%S).sql
+docker exec -t dle-cloud-db pg_dumpall -c -U cloud  > /mnt/md1/services/server/cloud-db-dumps/$(date +%Y-%m-%d_%H_%M_%S).sql
 wait
 docker compose exec --user www-data cloud  php occ maintenance:mode --off
 wait
