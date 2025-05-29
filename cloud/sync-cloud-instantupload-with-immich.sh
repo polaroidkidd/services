9c7c85d54e14900cc8344f2b25d6c33042dd0555
Daniel Einars <contact@dle.dev>
2025-05-29 16:09:45 +0200

    update sync script

--- cloud/sync-cloud-instantupload-with-immich.sh
+++ cloud/sync-cloud-instantupload-with-immich.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Service Script to sync the InstantUpload folder with the Immich library
-
 WATCH_DIR="/mnt/md1/services/server/volumes/cloud/data/daniel.einars/files/InstantUpload/Camera"
 DEST_DIR="/mnt/md1/services/server/volumes/immich/library/external/camera"
 
 while true; do
   inotifywait -e close_write,create,delete,move -r "$WATCH_DIR"
-  rsync --info=progress2 --info=name0 --stats --ignore-existing -v --dry-run "$WATCH_DIR"/* "$DEST_DIR"/
+  rsync \
+    --recursive \
+    --info=progress2 \
+    --info=name0 \
+    --stats \
+    --verbose \
+    --delete \
+    --dry-run \
+    "$WATCH_DIR"/ "$DEST_DIR"/
 done
